178feabadf21f92aa1e809e5b71e6f6ba408b094
Dunya Kirkali <dunyakirkali@gmail.com>
2020-06-09 20:56:37 +0200

    Cleaning up

--- lib/firestore.ex
+++ lib/firestore.ex
@@ -1,62 +1,72 @@
 defmodule Firestore do
   alias Goth.Token
+  alias Google.Firestore.V1
 
-  def hello do
-    # Get a token
-    {:ok, token} = Token.for_scope("https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/datastore")
-    |> IO.inspect
+  defp get_token do
+    Token.for_scope("https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/datastore")
+  end
+
+  defp create_channel do
+    {:ok, token} = get_token()
 
     # Read CA certificate for certificate pinning
     root = File.cwd!
-    ca_path =
-      "#{root}/priv/certs/edgecert-googleapis-com.pem"
-      |> IO.inspect
+    ca_path = "#{root}/priv/certs/edgecert-googleapis-com.pem"
 
     # Prepare options for the client
     opts = [
-      #interceptors: [GRPC.Logger.Client],
+      interceptors: [GRPC.Logger.Client],
       cred: GRPC.Credential.new(ssl: [cacertfile: ca_path]),
       headers: [
         "Authorization": "#{token.type} #{token.token}" 
       ]
     ]
     # Create the client
-    {:ok, channel} = GRPC.Stub.connect("firestore.googleapis.com:443", opts)
+    GRPC.Stub.connect("firestore.googleapis.com:443", opts)
+  end
 
-    # Get Collection Ids
-    request = Google.Firestore.V1.ListCollectionIdsRequest.new(
+  def list_collection_ids(channel) do
+    request = V1.ListCollectionIdsRequest.new(
       parent: "projects/gele-b64ed/databases/(default)/documents",
       page_size: 10
     )
 
-    {:ok, reply} = channel |> Google.Firestore.V1.Firestore.Stub.list_collection_ids(request, content_type: "application/grpc")
+    channel
+    |> V1.Firestore.Stub.list_collection_ids(request, content_type: "application/grpc")
+  end
+
+  def hello do
+    {:ok, channel} = create_channel()
+    {:ok, reply} = list_collection_ids(channel)
 
     # Get Documents from the first collection
     last_collection = List.last(reply.collection_ids)
-    request = Google.Firestore.V1.ListDocumentsRequest.new(
+    request = V1.ListDocumentsRequest.new(
       parent: "projects/gele-b64ed/databases/(default)/documents",
-      page_size: 1000,
+      page_size: 10_000,
       collection_id: last_collection
     )
 
-    {:ok, reply} = channel |> Google.Firestore.V1.Firestore.Stub.list_documents(request, content_type: "application/grpc")
+    {:ok, reply} = channel |> V1.Firestore.Stub.list_documents(request, content_type: "application/grpc")
+                   |> IO.inspect
 
     # Create documents
-    Enum.each(0..10_000, fn _ ->
-      uuid = UUID.uuid1()
-      value = Google.Firestore.V1.Value.new(
-        value_type: {:string_value, uuid}
-      )
-      document = Google.Firestore.V1.Document.new(
-        fields: %{"name" => value} 
-      )
-      request = Google.Firestore.V1.CreateDocumentRequest.new(
-        parent: "projects/gele-b64ed/databases/(default)/documents",
-        collection_id: "vehicles",
-        document: document
-      )
+    # Enum.each(0..10_000, fn _ ->
+    #   uuid = UUID.uuid1()
+    #   value = V1.Value.new(
+    #     value_type: {:string_value, uuid}
+    #   )
+    #   document = V1.Document.new(
+    #     fields: %{"name" => value} 
+    #   )
+    #   request = V1.CreateDocumentRequest.new(
+    #     parent: "projects/gele-b64ed/databases/(default)/documents",
+    #     collection_id: "vehicles",
+    #     document: document
+    #   )
 
-      {:ok, reply} = channel |> Google.Firestore.V1.Firestore.Stub.create_document(request, content_type: "application/grpc")
-    end)
+    #   {:ok, reply} = channel |> V1.Firestore.Stub.create_document(request, content_type: "application/grpc")
+    # end)
+    Enum.count(reply.documents)
   end
 end
