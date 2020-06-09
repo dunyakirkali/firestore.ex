5c991c00ced0b3d4678c246c43b142e79ca8cbf2
Dunya Kirkali <dunyakirkali@gmail.com>
2020-06-09 19:21:42 +0200

    Add sample document creation

--- lib/firestore.ex
+++ lib/firestore.ex
@@ -14,7 +14,7 @@ defmodule Firestore do
 
     # Prepare options for the client
     opts = [
-      interceptors: [GRPC.Logger.Client],
+      #interceptors: [GRPC.Logger.Client],
       cred: GRPC.Credential.new(ssl: [cacertfile: ca_path]),
       headers: [
         "Authorization": "#{token.type} #{token.token}" 
@@ -35,14 +35,28 @@ defmodule Firestore do
     last_collection = List.last(reply.collection_ids)
     request = Google.Firestore.V1.ListDocumentsRequest.new(
       parent: "projects/gele-b64ed/databases/(default)/documents",
-      page_size: 10,
+      page_size: 1000,
       collection_id: last_collection
     )
 
     {:ok, reply} = channel |> Google.Firestore.V1.Firestore.Stub.list_documents(request, content_type: "application/grpc")
 
-    Enum.map(reply.documents, fn document ->
-      IO.inspect(document.fields)
+    # Create documents
+    Enum.each(0..10_000, fn _ ->
+      uuid = UUID.uuid1()
+      value = Google.Firestore.V1.Value.new(
+        value_type: {:string_value, uuid}
+      )
+      document = Google.Firestore.V1.Document.new(
+        fields: %{"name" => value} 
+      )
+      request = Google.Firestore.V1.CreateDocumentRequest.new(
+        parent: "projects/gele-b64ed/databases/(default)/documents",
+        collection_id: "vehicles",
+        document: document
+      )
+
+      {:ok, reply} = channel |> Google.Firestore.V1.Firestore.Stub.create_document(request, content_type: "application/grpc")
     end)
   end
 end
